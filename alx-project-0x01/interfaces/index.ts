0ec0b39cefda4468835eab7a0039c03a87349ed5
Tsedenawit <tsedenawit3@gmail.com>
2025-07-20 16:35:25 -0400

    send

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -4,3 +4,33 @@ export interface PostProps {
   title: string;
   body: string;
 }
+
+export interface UserProps{
+    id:number;
+    name:string;
+    username:string;
+    email:string;
+    address:Address;
+    phone:number;
+    website:string;
+    company:Company
+}
+
+export interface Address{
+    street:string;
+    suite:string;
+    city:string;
+    zipcode:number;
+    geo:Geo;
+}
+
+export interface Geo{
+    lat:number;
+    lng:number;
+}
+
+export interface Company{
+    name:string;
+    catchPhrase:string;
+    bs:string
+}
