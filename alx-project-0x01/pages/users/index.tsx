2438e38568e7f6e0d70a8da391ed2ead511368ac
Tsedenawit <tsedenawit3@gmail.com>
2025-07-20 16:13:30 -0400

    new

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,9 @@
+const indexs: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-7xl font-thin">Welcome Page</h1>
+    </div>
+  )
+}
+
+ export default indexs;
